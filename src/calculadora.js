882bd6c400db1686e4899f201b255199bd6d1088
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2026-05-21 14:39:57 -0400

    make custom separator test pass

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,21 @@
 function calcularCadena(cadena) {
+  let delimitador = ',';
+
+  if (cadena.startsWith('//')) {
+    const partes = cadena.split(' ');
+    const encabezado = partes[0];
+
+    delimitador = encabezado[3];
+    cadena = partes[1];
+  }
+
   cadena = cadena.replace(/-/g, ',');
+  cadena = cadena.replaceAll(delimitador, ',');
 
   return cadena
     .split(',')
     .reduce((total, n) => total + Number(n), 0);
 }
+
 
 export default calcularCadena;
