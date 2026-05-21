fea0a3f32e42f9536ece2c8d26315c9b40a356ec
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2026-05-21 14:47:57 -0400

    refactor

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,23 +1,31 @@
-function calcularCadena(cadena) {
-  let delimitador = ',';
+function normalizarSeparadores(cadena, delimitador) {
+  return cadena
+    .replace(/-/g, ',')
+    .replaceAll(delimitador, ',');
+}
 
-  if (cadena.startsWith('//')) {
-    const partes = cadena.split(' ');
-    const encabezado = partes[0];
-
-    delimitador = encabezado[3];
-    cadena = partes[1];
+function obtenerDelimitador(cadena) {
+  if (!cadena.startsWith('//')) {
+    return { delimitador: ',', numeros: cadena };
   }
 
-  cadena = cadena.replace(/-/g, ',');
-  cadena = cadena.replaceAll(delimitador, ',');
+  const partes = cadena.split(' ');
+  const encabezado = partes[0];
 
-  return cadena
+  return {
+    delimitador: encabezado[3],
+    numeros: partes[1]
+  };
+}
+
+function calcularCadena(cadena) {
+  const { delimitador, numeros } = obtenerDelimitador(cadena);
+
+  return normalizarSeparadores(numeros, delimitador)
     .split(',')
     .map(Number)
     .filter(n => n <= 1000)
     .reduce((total, n) => total + n, 0);
 }
-
 
 export default calcularCadena;
