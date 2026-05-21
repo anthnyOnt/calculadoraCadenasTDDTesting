e91d9a32add214405b166bcd85e30101e97bc869
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2026-05-21 13:51:03 -0400

    numbers separated by comma test failed

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -3,3 +3,7 @@ import calcularCadena from "./calculadora";
 test('retorna el mismo número cuando recibe uno solo', () => {
   expect(calcularCadena("5")).toBe(5);
 });
+
+test('suma dos números separados por coma', () => {
+  expect(add("1,2")).toBe(3);
+});
